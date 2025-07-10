dbaf2a13ff8e0bb12379800d213f83e755098e2a
jasmineyyip <yippie.web@gmail.com>
2025-07-09 23:53:25 -0700

    styling fix

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,9 +7,6 @@ const AboutMe = () => {
             <h2 className="section-title">About</h2>
             <p className="about-me-text">
                 Hi! I'm Jasmine, a rising junior studying Computer Science at the University of Southern California (USC) passionate about building products at the intersection of technology, creativity, and social impact.
-            </p>
-            <br></br>
-            <p className="about-me-text">
                 This summer, I am interning at LinkedIn as a software engineer. Previously, I've developed apps and softwares at a speech therapy startup helping children with speech disorders and at a DJ tech company empowering music creators. I love exploring how engineering can empower human connection and self-expression.
             </p>
         </div>
